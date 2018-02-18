3daca3d275144c9ecf1079c4dd2f020081cc6b00
Mor Kenane <morkenan@hotmail.com>
2018-02-18 00:22:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,11 @@ end
 
 def long_planeteer_calls(array)
   array.any? do |call|
-    call.length > 4
+  call.length > 4
   end 
-  
 end
+
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
