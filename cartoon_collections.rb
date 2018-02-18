3d4978e1d984201e9357979730cd5a69a6af567a
Mor Kenane <morkenan@hotmail.com>
2018-02-18 01:25:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,11 +22,9 @@ def long_planeteer_calls(array)
 end
 
 
-
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-   array.find do |cheese|
-     cheese_types.include?(cheese)
+  array.find do |cheese|
+  cheese_types.include?(cheese)
    end 
 end
